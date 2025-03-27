54698c93735bdbb70aa3220eba0163c44fdc58e8
Prathamesh Sankhe <pmsankhe_b23@it.vjti.ac.in>
2025-03-27 13:15:23 +0530

    android init

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+export default {
+  appId: 'com.growguide.app',
+  appName: 'GrowGuide',
+  webDir: '.next',
+  server: {
+    url: 'http://192.168.45.111:3000', // For development with live server
+    cleartext: true, // Allow HTTP for local testing
+  },
+};
